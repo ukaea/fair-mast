02dc0c11af4e7586b7b254a049b7bca5be92e428
Nathan Cummings <nathan.cummings@ukaea.uk>
2022-07-19 12:08:19 +0100

    Formatting updates

--- mast2HDF5.py
+++ mast2HDF5.py
@@ -1,16 +1,13 @@
 import datetime
-import os
 import random
 import time
-from functools import partial
-from multiprocessing import current_process, Pool
+from multiprocessing import Pool, current_process
 
 import h5py
-from tqdm import tqdm
-
 import pyuda
 from mast.mast_client import ListType
 from pycpf import pycpf
+from tqdm import tqdm
 
 
 def write_file(shot):
@@ -24,29 +21,27 @@ def write_file(shot):
     aliases = set([signal.source_alias for signal in signals])
     sources = [source for source in sources if source.source_alias in aliases]
     source_dict = {
-            source: set([
-                signal.signal_name
-                for signal in signals 
-                if signal.source_alias == source
-            ])
-            for source in aliases
+        source: set(
+            [signal.signal_name for signal in signals if signal.source_alias == source]
+        )
+        for source in aliases
     }
 
-    with h5py.File(file_path, 'a') as file:
+    with h5py.File(file_path, "a") as file:
         cpf = file.create_group("cpf")
         cpf_categories = pycpf.columns()
         for entry in tqdm(
-                cpf_categories,
-                desc=f"Converting cpf for shot {shot}",
-                position=process,
-                leave=False,
+            cpf_categories,
+            desc=f"Converting cpf for shot {shot}",
+            position=process,
+            leave=False,
         ):
             cpf_entry = pycpf.query(entry[0], f"shot = {shot}")
             if cpf_entry:
                 try:
                     cpf_data = cpf.create_dataset(
-                            entry[0],
-                            data=cpf_entry[entry[0]][0],
+                        entry[0],
+                        data=cpf_entry[entry[0]][0],
                     )
                     cpf_data.attrs["description"] = entry[1]
                 except Exception as e:
@@ -63,16 +58,16 @@ def write_file(shot):
             group.attrs["signal_type"] = source.type
 
         for source, signal_list in tqdm(
-                source_dict.items(),
-                desc=f"Converting signal data for shot {shot}",
-                position=process * 2,
-                leave=False,
+            source_dict.items(),
+            desc=f"Converting signal data for shot {shot}",
+            position=process * 2,
+            leave=False,
         ):
             for signal_name in tqdm(
-                    signal_list,
-                    desc=f"Converting {source}",
-                    position=process * 2 + 1,
-                    leave=False,
+                signal_list,
+                desc=f"Converting {source}",
+                position=process * 2 + 1,
+                leave=False,
             ):
                 try:
                     data = client.get(signal_name, shot)
@@ -81,7 +76,7 @@ def write_file(shot):
                     continue
 
                 if type(data) == pyuda._signal.Signal:
-                    if signal_name.startswith('/'):
+                    if signal_name.startswith("/"):
                         signal_alias = signal_name[5:]
                     else:
                         signal_alias = signal_name[4:]
@@ -93,96 +88,85 @@ def write_file(shot):
                     group.attrs["pass_date"] = data.meta["pass_date"]
                     dataset = group.create_dataset("data", data=data.data)
                     dataset.attrs["units"] = data.units
-                    errors = group.create_dataset("errors", data=data.errors)
+                    group.create_dataset("errors", data=data.errors)
                     if data.time:
-                        time = group.create_dataset(
-                                "time", data=data.time.data
-                        )
+                        time = group.create_dataset("time", data=data.time.data)
                         time.attrs["units"] = data.time.units
 
         images = file.create_group("images")
         for image_source in tqdm(
-                image_sources,
-                desc=f"Converting image data for shot {shot}.",
-                position=process,
+            image_sources,
+            desc=f"Converting image data for shot {shot}.",
+            position=process,
         ):
             if (image_source.format == "TIF") or (image_source.source_alias == "rcc"):
                 continue
             image_data = client.get_images(image_source.source_alias, shot)
             image_group = images.create_group(image_source.source_alias)
             attributes = {
-                    "board_temp": image_data.board_temp,
-                    "bottom": image_data.bottom,
-                    "camera": image_data.camera,
-                    "ccd_temp": image_data.ccd_temp,
-                    "codex": image_data.codex,
-                    "date_time": image_data.date_time,
-                    "depth": image_data.depth,
-                    "exposure": image_data.exposure,
-                    "filter": image_data.filter,
-                    "gain": image_data.gain,
-                    "hbin": image_data.hbin,
-                    "height": image_data.height,
-                    "is_color": image_data.is_color,
-                    "left": image_data.left,
-                    "lens": image_data.lens,
-                    "n_frames": image_data.n_frames,
-                    "offset": image_data.offset,
-                    "orientation": image_data.orientation,
-                    "pre_exp": image_data.pre_exp,
-                    "right": image_data.right,
-                    "shot": image_data.shot,
-                    "strobe": image_data.strobe,
-                    "taps": image_data.taps,
-                    "top": image_data.top,
-                    "trigger": image_data.trigger,
-                    "vbin": image_data.vbin,
-                    "view": image_data.view,
-                    "width": image_data.width,
+                "board_temp": image_data.board_temp,
+                "bottom": image_data.bottom,
+                "camera": image_data.camera,
+                "ccd_temp": image_data.ccd_temp,
+                "codex": image_data.codex,
+                "date_time": image_data.date_time,
+                "depth": image_data.depth,
+                "exposure": image_data.exposure,
+                "filter": image_data.filter,
+                "gain": image_data.gain,
+                "hbin": image_data.hbin,
+                "height": image_data.height,
+                "is_color": image_data.is_color,
+                "left": image_data.left,
+                "lens": image_data.lens,
+                "n_frames": image_data.n_frames,
+                "offset": image_data.offset,
+                "orientation": image_data.orientation,
+                "pre_exp": image_data.pre_exp,
+                "right": image_data.right,
+                "shot": image_data.shot,
+                "strobe": image_data.strobe,
+                "taps": image_data.taps,
+                "top": image_data.top,
+                "trigger": image_data.trigger,
+                "vbin": image_data.vbin,
+                "view": image_data.view,
+                "width": image_data.width,
             }
             for key, value in attributes.items():
                 image_group.attrs[key] = value
 
-            frame_times = image_group.create_dataset(
-                    "frame_times",
-                    data=image_data.frame_times,
+            image_group.create_dataset(
+                "frame_times",
+                data=image_data.frame_times,
             )
             frames_group = image_group.create_group("frames")
             for frame in image_data.frames:
-                frame_group = frames_group.create_group(
-                        f"frame_{frame.number}"
-                )
+                frame_group = frames_group.create_group(f"frame_{frame.number}")
                 keys = dir(frame)
-                keys = [key for key in keys if not key.startswith('_')]
+                keys = [key for key in keys if not key.startswith("_")]
                 for key in keys:
-                    frame_data = frame_group.create_dataset(
-                            key, data=getattr(frame, key)
-                    )
+                    frame_group.create_dataset(key, data=getattr(frame, key))
+
 
 if __name__ == "__main__":
     start_time = time.time()
     shot_list = [
-            29976,
-            30420,
-            30422,
-            30426,
-            30430,
-            30449,
-            30464,
-            30468,
-            30469,
-            30471,
+        29976,
+        30420,
+        30422,
+        30426,
+        30430,
+        30449,
+        30464,
+        30468,
+        30469,
+        30471,
     ]
     processes = 2
     with Pool(processes) as pool:
-        pool.map(
-            write_file,
-            random.sample(shot_list, processes) 
-        )
+        pool.map(write_file, random.sample(shot_list, processes))
 
     execution_time = time.time() - start_time
-    with open("times.txt", 'a') as file:
-        file.write(
-            f"{processes},{datetime.timedelta(seconds=execution_time)}\n"
-        )
- 
+    with open("times.txt", "a") as file:
+        file.write(f"{processes},{datetime.timedelta(seconds=execution_time)}\n")
