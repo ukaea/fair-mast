f1dd714063d09b203af8cc70b23b683fc75072e4
Edward Harrington <hs4081@freia021.hpc.l>
2022-10-14 15:53:58 +0100

    black formatting output_test

--- output_test.py
+++ output_test.py
@@ -1,8 +1,8 @@
-#----------------------------------------
+# ----------------------------------------
 # Pytest to compare two .h5 files.
 # For best results run using:
 # > pytest PATH/output_test.py -s -vv
-#----------------------------------------
+# ----------------------------------------
 
 
 import numpy as np
@@ -21,12 +21,14 @@ def expected_data(request):
     request.addfinalizer(file.close)
     return file
 
+
 @pytest.fixture(scope="session")
 def input_data(request):
     path = "/scratch/hs4081/30405.h5"
     file = h5py.File(path, "r")
     request.addfinalizer(file.close)
     return file
+
 
 @pytest.fixture
 def get_random_signal(expected_data):
@@ -37,11 +39,14 @@ def get_random_signal(expected_data):
     while type(obj) != h5py.Dataset:
         try:
             parent_obj = [*signal_path.values()][-1]
-            name = random.choice([*parent_obj.keys()]) 
-            if name in [*signal_path.keys()]: raise RuntimeError("Duplicate key in signal path")   #temporary error to stop infinite looping    
+            name = random.choice([*parent_obj.keys()])
+            if name in [*signal_path.keys()]:
+                raise RuntimeError(
+                    "Duplicate key in signal path"
+                )  # temporary error to stop infinite looping
             obj = parent_obj[name]
             signal_path[name] = obj
-        except IndexError: #if group has no child groups or datasets, reset source and start again.
+        except IndexError:  # if group has no child groups or datasets, reset source and start again.
             signal_path = {}
             source_name = random.choice([*expected_data.keys()])
             obj = expected_data[source_name]
@@ -49,11 +54,13 @@ def get_random_signal(expected_data):
     return signal_path
 
 
-
 def test_base_attrs(expected_data, input_data):
     assert expected_data.attrs == input_data.attrs
 
+
 NO_OF_REPEATS = 100
+
+
 @pytest.mark.parametrize("repeat_count", range(NO_OF_REPEATS))
 def test_random_sample(expected_data, input_data, get_random_signal, repeat_count):
     print("Testing " + str([*get_random_signal.keys()]))
@@ -65,7 +72,7 @@ def test_random_sample(expected_data, input_data, get_random_signal, repeat_coun
 
     expected_signal_data = [*get_random_signal.values()][-1]
     input_signal_data = signal_path[-1]
-    expected_signal_data = np.nan_to_num(expected_signal_data, copy = False)
-    input_signal_data = np.nan_to_num(input_signal_data, copy = False)
+    expected_signal_data = np.nan_to_num(expected_signal_data, copy=False)
+    input_signal_data = np.nan_to_num(input_signal_data, copy=False)
     assert (expected_signal_data[()] == input_signal_data[()]).all()
     print("Complete")
