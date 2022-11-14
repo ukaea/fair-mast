7fca730157796a0c22516fc2c190c465555e18f6
Edward Harrington <hs4081@freia020.hpc.l>
2022-11-14 12:06:15 +0000

    Reworked tests to allow for command line args

---
+++ output_test/conftest.py
@@ -0,0 +1,46 @@
+import os
+
+import pytest
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--ref",
+        action="store",
+        default=None,
+        help="Path to reference data. overwrites --shot",
+    )
+    parser.addoption(
+        "--input",
+        action="store",
+        default=None,
+        help="Path to data to test. overwrites --shot",
+    )
+    parser.addoption(
+        "--shot",
+        action="store",
+        default="30120",
+        help="Shot to process. From default directory, i.e: /home/hs4081/References/(shot).h5 and\or output/(shot.h5)",
+    )
+
+
+@pytest.fixture(scope="session")
+def get_expected_path(request):
+    DEFAULT_PATH = "/home/hs4081/References/"
+
+    shot = request.config.getoption("--shot")
+    path = request.config.getoption("--ref")
+    if path is None:
+        path = os.path.join(DEFAULT_PATH, f"{shot}.h5")
+    return path
+
+
+@pytest.fixture(scope="session")
+def get_input_path(request):
+    DEFAULT_PATH = "output/"
+
+    shot = request.config.getoption("--shot")
+    path = request.config.getoption("--input")
+    if path is None:
+        path = os.path.join(DEFAULT_PATH, f"{shot}.h5")
+    return path
