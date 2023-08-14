54a343c15102c1af5d82226806dd13e07d28c82b
Samuel Jackson <samuel.jackson@stfc.ac.uk>
2023-08-14 21:05:59 +0000

    Dockerize the api

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # README
+# FAIR MAST Archive
+
+## Deployment
+
+```bash
+docker-compose up -d --build
+```
--- src/api/environment.py
+++ src/api/environment.py
@@ -1,4 +1,8 @@
+import os
+
+host = os.environ.get("DATABASE_HOST", "localhost")
+
 # Location of the database
-SQLALCHEMY_DATABASE_URL = "postgresql://root:root@localhost:5432/mast_db"
+SQLALCHEMY_DATABASE_URL = f"postgresql://root:root@{host}:5432/mast_db"
 # Echo SQL statements
-SQLALCHEMY_DEBUG = False
+SQLALCHEMY_DEBUG = os.environ.get("SQLALCHEMY_DEBUG", False)
