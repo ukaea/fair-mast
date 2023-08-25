b8a25cf79120b924d0fa835cde6036af382bef6c
Samuel Jackson <samuel.jackson@stfc.ac.uk>
2023-08-25 10:11:45 +0000

    Update metadata script

--- src/metadata/create_signal_metadata.py
+++ src/metadata/create_signal_metadata.py
@@ -8,22 +8,37 @@ import numpy as np
 from rich.progress import track
 from netCDF4 import Dataset
 
-def parse_signal_metadata(path):
-    dataset = Dataset(path, mode='r')
+
+def read_netcdf(path):
+    dataset = Dataset(path, mode="r")
     # signal_root = zarr.open(path, mode='r')
     shot_nums = list(dataset.groups.keys())
     metadata = dataset[shot_nums[0]].__dict__
+    return shot_nums, metadata
+
+
+def read_zarr(path):
+    dataset = zarr.open_consolidated(path)
+    shot_nums = list(dataset.group_keys())
+    metadata = dict(next(dataset.groups())[1].attrs)
+    return shot_nums, metadata
+
+
+def parse_signal_metadata(path):
+    if path.suffix == ".nc":
+        shot_nums, metadata = read_netcdf(path)
+    elif path.suffix == ".zarr":
+        shot_nums, metadata = read_zarr(path)
 
     item = {}
-    item['shot_nums'] = shot_nums
-    item['name'] = path.stem
-    item['uri'] = str(path)
+    item["shot_nums"] = shot_nums
+    item["name"] = path.stem.upper()
+    item["uri"] = str(path)
     item.update(metadata)
-    print(path, len(shot_nums), item['shape'])
     return item
 
 
-def parse_metadata(paths, metadata_dir):
+def parse_metadata(paths, output_file):
     pool = mp.Pool(8)
     mapper = pool.map(parse_signal_metadata, paths)
 
@@ -31,22 +46,18 @@ def parse_metadata(paths, metadata_dir):
     for item in track(mapper, total=len(paths)):
         metadata.append(item)
     metadata = pd.DataFrame(metadata)
-    metadata['shape'] = metadata['shape'].map(lambda x: np.atleast_1d(x).tolist())
-    print(metadata['shape'])
-    metadata.to_parquet(metadata_dir / 'signal_metadata.parquet')
+    print(metadata)
+    metadata.to_parquet(output_file)
 
 
 @click.command()
-@click.argument('data_dir')
-@click.argument('metadata_dir')
-def main(data_dir, metadata_dir):
+@click.argument("data_dir")
+@click.argument("output_file")
+def main(data_dir, output_file):
     data_dir = Path(data_dir)
-    metadata_dir = Path(metadata_dir)
-
-    signal_files = list(sorted(data_dir.glob('*.nc')))
-    print(len(signal_files))
+    signal_files = list(sorted(data_dir.glob("*.zarr")))
     signal_files = signal_files
-    parse_metadata(signal_files, metadata_dir)
+    parse_metadata(signal_files, output_file)
 
 
 if __name__ == "__main__":
