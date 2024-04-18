7a48bb8e9346aaebd7be9949e06fe8fab17244c1
Samuel Jackson <samuel.jackson@stfc.ac.uk>
2024-04-18 15:56:54 +0100

    Update job script

--- src/archive/main2.py
+++ src/archive/main2.py
@@ -3,14 +3,7 @@ import logging
 from dask_mpi import initialize
 from src.archive.uploader import UploadConfig
 from src.archive.workflow import WorkflowManager
-
-
-def read_shot_file(shot_file: str) -> list[int]:
-    with open(shot_file) as f:
-        shot_nums = f.readlines()[1:]
-        shot_nums = map(lambda x: x.strip(), shot_nums)
-        shot_nums = list(sorted(map(int, shot_nums)))
-    return shot_nums
+from src.archive.utils import read_shot_file
 
 
 def main():
