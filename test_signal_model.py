cf53ffec07f19c8c6d0a92f6264f9220b2302820
jameshod5 <james.hodson@ukaea.uk>
2024-05-09 10:58:02 +0100

    creation and teardown of database in fixture

--- test_signal_model.py
+++ test_signal_model.py
@@ -17,94 +17,84 @@ from sqlalchemy_utils.functions import (
 )
 from pathlib import Path
 from tqdm import tqdm
-from data_creation_for_test import create_cpf_summary, create_scenarios, create_shots, create_signals, create_sources, create_shot_source_links
+from data_creation_for_test import create_cpf_summary, create_scenarios, create_shots, create_signals, create_sources
 
 data_path = "/Users/pstanis/Documents/work_projects/fair-mast/data/metadata/prod"
-data_path = Path(data_path)
 
-# create the test database
+# Set up the database URL
 host = os.environ.get("DATABASE_HOST", "localhost")
 SQLALCHEMY_DATABASE_TEST_URL = f"postgresql://root:root@{host}:5432/test_db"
-LAST_MAST_SHOT = 30471
 
-if database_exists(SQLALCHEMY_DATABASE_TEST_URL):
+# Fixture to create and drop the database
+@pytest.fixture(scope="session")
+def test_db():
+    if database_exists(SQLALCHEMY_DATABASE_TEST_URL):
+        drop_database(SQLALCHEMY_DATABASE_TEST_URL)
+    create_database(SQLALCHEMY_DATABASE_TEST_URL)
+
+    print("\n ---- Creating Test Database ----- \n")
+    engine = create_engine(SQLALCHEMY_DATABASE_TEST_URL)
+    SQLModel.metadata.create_all(engine)
+    TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+    create_cpf_summary(SQLALCHEMY_DATABASE_TEST_URL, Path(data_path))
+    create_scenarios(SQLALCHEMY_DATABASE_TEST_URL, Path(data_path))
+    create_shots(SQLALCHEMY_DATABASE_TEST_URL, Path(data_path))
+    create_signals(SQLALCHEMY_DATABASE_TEST_URL, Path(data_path), num_signals=1)
+    create_sources(SQLALCHEMY_DATABASE_TEST_URL, Path(data_path))
+
+    yield TestingSessionLocal()
+
+    print("\n ---- Testing Complete: Closing Database ----- \n")
+
     drop_database(SQLALCHEMY_DATABASE_TEST_URL)
-create_database(SQLALCHEMY_DATABASE_TEST_URL)
 
-engine = create_engine(SQLALCHEMY_DATABASE_TEST_URL)
-TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+    print("---- Database Closed -----")
 
-# need this to make sure we don't use the other app when using the API
-def override_get_db():
-    try:
-        db = TestingSessionLocal()
-        yield db
-    finally:
-        db.close()
-app.dependency_overrides[get_db] = override_get_db
+# Fixture to override the database dependency
+@pytest.fixture
+def override_get_db(test_db):
+    def override():
+        try:
+            db = test_db
+            yield db
+        finally:
+            db.close()
 
+    app.dependency_overrides[get_db] = override
+
+# Fixture to create a client for testing
+@pytest.fixture(scope="module")
+def client():
+    with TestClient(app) as client:
+        yield client
 
 # ========= Tests ==========
 
-@pytest.fixture(scope="module")
-def client():
-    client = TestClient(app)
-    return client
-
-
-# this is needed for the create_all_part, so keep test_source_valid or need to figure this bit out again
-@pytest.fixture(scope="module")
-def db_session():
-    SQLModel.metadata.create_all(engine)
-    session = TestingSessionLocal()
-    yield session
-    session.rollback()
-    session.close()
-
-def test_source_valid(db_session):
-    valid_source = SourceModel(
-        doi=None,
-        name="test",
-        description="test",
-        source_type="Analysed"
-        )
-    db_session.add(valid_source)
-    db_session.commit()
-    sample_article = db_session.query(SourceModel).first()
-    item = sample_article.dict(exclude_none=True)
-    assert sample_article.name == "test"
-    assert len(item) == 3
-
-def test_get_cpf(client):
-    create_cpf_summary(SQLALCHEMY_DATABASE_TEST_URL, data_path)
-    response = client.get("json/cpf_summary")
-    data = response.json()
+def test_get_cpf(client, override_get_db):
+    response = client.get("/json/cpf_summary")
     assert response.status_code == 200
+    data = response.json()
     assert len(data) == 265
 
-def test_get_scenarios(client):
-    create_scenarios(SQLALCHEMY_DATABASE_TEST_URL, data_path)
+def test_get_scenarios(client, override_get_db):
     response = client.get("json/scenarios")
     data = response.json()
     assert response.status_code == 200
     
-def test_get_shots(client):
-    create_shots(SQLALCHEMY_DATABASE_TEST_URL, data_path)
+def test_get_shots(client, override_get_db):
     response = client.get("json/shots")
     data = response.json()
     assert response.status_code == 200
 
 # will only be signals for shot 25877, can change number of shots in data_creation_for_test.py
-def test_get_signals(client):
-    create_signals(SQLALCHEMY_DATABASE_TEST_URL, data_path, num_signals=1)
+def test_get_signals(client, override_get_db):
     response = client.get("json/signals")
     data = response.json()
     assert response.status_code == 200
     assert data[0]['shot_id'] == 25877
 
-def test_get_sources(client):
-    create_sources(SQLALCHEMY_DATABASE_TEST_URL, data_path)
+def test_get_sources(client, override_get_db):
     response = client.get("json/sources")
     data = response.json()
     assert response.status_code == 200
-    print(data)
