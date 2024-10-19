ba18dccbf046d9a9eff53146bf4a6b6a94a937e5
Nathan Cummings <nathan.cummings@hotmail.co.uk>
2024-10-19 13:31:00 +0100

    Remove a whole bunch of stuff.

--- src/archive/utils.py
+++
@@ -1,6 +0,0 @@
-def read_shot_file(shot_file: str) -> list[int]:
-    with open(shot_file) as f:
-        shot_nums = f.readlines()[1:]
-        shot_nums = map(lambda x: x.strip(), shot_nums)
-        shot_nums = list(sorted(map(int, shot_nums)))
-    return shot_nums
