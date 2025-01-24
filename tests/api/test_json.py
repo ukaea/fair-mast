1cdc89227f5f9c86bd06df0bba2e889506cffcec
khalid <khalid619lawal@gmail.com>
2025-01-24 16:13:56 +0000

    Add: Keycloak config file

--- tests/api/test_json.py
+++ tests/api/test_json.py
@@ -159,119 +159,119 @@ def test_exception_handler(client, override_get_db):
     ]
 
 
-# def test_post_shots(client):
-#     endpoint = "http://localhost:8081/json/shots"
+def test_post_shots(client):
+    endpoint = "http://localhost:8081/json/shots"
 
-#     payload = {
-#         "shot_id": 90121,
-#         "uuid": "883382e6-df26-55f2-af85-ad7bdec24835",
-#         "url": "s3://mast/level1/shots/30122.zarr",
-#         "timestamp": "2013-09-09T14:24:00",
-#         "preshot_description": "extend CHFS to 500 ms - increase zref to +0.75 - ramp current from set value of 0.5 at 220ms to a set value of 0.75 at 310ms",
-#         "postshot_description": "1 breakdown on SS at 100 ms - good deep H-mode - disrupts during the current ramp - but still in H-mode",
-#         "campaign": "M9",
-#         "reference_shot": 29495,
-#         "scenario": 3,
-#         "heating": "2 Beams,SS Beam,SW Beam",
-#         "pellets": "false",
-#         "rmp_coil": "false",
-#         "current_range": "700 kA",
-#         "divertor_config": "Conventional",
-#         "plasma_shape": "Connected Double Null",
-#         "comissioner": "null",
-#         "facility": "MAST",
-#     }
+    payload = {
+        "shot_id": 90121,
+        "uuid": "883382e6-df26-55f2-af85-ad7bdec24835",
+        "url": "s3://mast/level1/shots/30122.zarr",
+        "timestamp": "2013-09-09T14:24:00",
+        "preshot_description": "extend CHFS to 500 ms - increase zref to +0.75 - ramp current from set value of 0.5 at 220ms to a set value of 0.75 at 310ms",
+        "postshot_description": "1 breakdown on SS at 100 ms - good deep H-mode - disrupts during the current ramp - but still in H-mode",
+        "campaign": "M9",
+        "reference_shot": 29495,
+        "scenario": 3,
+        "heating": "2 Beams,SS Beam,SW Beam",
+        "pellets": "false",
+        "rmp_coil": "false",
+        "current_range": "700 kA",
+        "divertor_config": "Conventional",
+        "plasma_shape": "Connected Double Null",
+        "comissioner": "null",
+        "facility": "MAST",
+    }
 
-#     response = client.post(
-#         endpoint,
-#         auth=HTTPBasicAuth(username=TEST_USERNAME, password=TEST_PASSWORD),
-#         json=payload,
-#     )
-#     assert response.status_code == 200
-
-
-# def test_post_signals(client, override_get_db):
-#     endpoint = "/json/signals"
-
-#     payload = {
-#         "uuid": "005fe9da-964a-5563-af2c-dffe3d99ed89",
-#         "shot_id": 90121,
-#         "name": "alp/inner_lo_powpeakval",
-#         "version": 0,
-#         "rank": 1,
-#         "url": "s3://mast/level1/shots/30398.zarr/alp/inner_lo_powpeakval",
-#         "source": "alp",
-#         "quality": "Not Checked",
-#         "shape": [288],
-#         "provenance": "null",
-#         "units": "null",
-#         "description": "",
-#         "signal_type": "Analysed",
-#         "dimensions": ["time"],
-#     }
-
-#     response = client.post(
-#         endpoint,
-#         auth=HTTPBasicAuth(username=TEST_USERNAME, password=TEST_PASSWORD),
-#         json=payload,
-#     )
-#     assert response.status_code == 200
+    response = client.post(
+        endpoint,
+        auth=HTTPBasicAuth(username=TEST_USERNAME, password=TEST_PASSWORD),
+        json=payload,
+    )
+    assert response.status_code == 200
 
 
-# def test_post_sources(client, override_get_db):
-#     endpoint = "/json/sources"
+def test_post_signals(client, override_get_db):
+    endpoint = "/json/signals"
 
-#     payload = {
-#         "uuid": "a2ecc848-21bf-5137-bd8c-bfcf06020cc9",
-#         "shot_id": 90121,
-#         "name": "abm",
-#         "url": "s3://mast/level1/shots/30119.zarr/abm",
-#         "description": "multi-chord bolometers",
-#         "quality": "Not Checked",
-#     }
+    payload = {
+        "uuid": "005fe9da-964a-5563-af2c-dffe3d99ed89",
+        "shot_id": 90121,
+        "name": "alp/inner_lo_powpeakval",
+        "version": 0,
+        "rank": 1,
+        "url": "s3://mast/level1/shots/30398.zarr/alp/inner_lo_powpeakval",
+        "source": "alp",
+        "quality": "Not Checked",
+        "shape": [288],
+        "provenance": "null",
+        "units": "null",
+        "description": "",
+        "signal_type": "Analysed",
+        "dimensions": ["time"],
+    }
 
-#     response = client.post(
-#         endpoint,
-#         auth=HTTPBasicAuth(username=TEST_USERNAME, password=TEST_PASSWORD),
-#         json=payload,
-#     )
-#     assert response.status_code == 200
-
-
-# def test_post_scenarios(client, override_get_db):
-#     endpoint = "/json/scenarios"
-
-#     payload = {"id": 80, "name": "S1"}
-
-#     response = client.post(
-#         endpoint,
-#         auth=HTTPBasicAuth(username=TEST_USERNAME, password=TEST_PASSWORD),
-#         json=payload,
-#     )
-#     assert response.status_code == 200
+    response = client.post(
+        endpoint,
+        auth=HTTPBasicAuth(username=TEST_USERNAME, password=TEST_PASSWORD),
+        json=payload,
+    )
+    assert response.status_code == 200
 
 
-# def test_post_cpf_summary(client, override_get_db):
-#     endpoint = "/json/cpf_summary"
+def test_post_sources(client, override_get_db):
+    endpoint = "/json/sources"
 
-#     payload = {"index": 0, "name": "P12450", "description": "CP3c Set Volts"}
+    payload = {
+        "uuid": "a2ecc848-21bf-5137-bd8c-bfcf06020cc9",
+        "shot_id": 90121,
+        "name": "abm",
+        "url": "s3://mast/level1/shots/30119.zarr/abm",
+        "description": "multi-chord bolometers",
+        "quality": "Not Checked",
+    }
 
-#     response = client.post(
-#         endpoint,
-#         auth=HTTPBasicAuth(username=TEST_USERNAME, password=TEST_PASSWORD),
-#         json=payload,
-#     )
-#     assert response.status_code == 200
+    response = client.post(
+        endpoint,
+        auth=HTTPBasicAuth(username=TEST_USERNAME, password=TEST_PASSWORD),
+        json=payload,
+    )
+    assert response.status_code == 200
 
 
-# def test_unauthorized_post_scenarios(client, override_get_db):
-#     with pytest.raises(KeycloakAuthorizationConfigError):
-#         endpoint = "/json/scenarios"
+def test_post_scenarios(client, override_get_db):
+    endpoint = "/json/scenarios"
 
-#         payload = {"id": 85, "name": "S1"}
+    payload = {"id": 80, "name": "S1"}
 
-#         client.post(
-#             endpoint,
-#             auth=HTTPBasicAuth(username=UNAUTHORIZED_USER, password=TEST_PASSWORD),
-#             json=payload,
-#         )
+    response = client.post(
+        endpoint,
+        auth=HTTPBasicAuth(username=TEST_USERNAME, password=TEST_PASSWORD),
+        json=payload,
+    )
+    assert response.status_code == 200
+
+
+def test_post_cpf_summary(client, override_get_db):
+    endpoint = "/json/cpf_summary"
+
+    payload = {"index": 0, "name": "P12450", "description": "CP3c Set Volts"}
+
+    response = client.post(
+        endpoint,
+        auth=HTTPBasicAuth(username=TEST_USERNAME, password=TEST_PASSWORD),
+        json=payload,
+    )
+    assert response.status_code == 200
+
+
+def test_unauthorized_post_scenarios(client, override_get_db):
+    with pytest.raises(KeycloakAuthorizationConfigError):
+        endpoint = "/json/scenarios"
+
+        payload = {"id": 85, "name": "S1"}
+
+        client.post(
+            endpoint,
+            auth=HTTPBasicAuth(username=UNAUTHORIZED_USER, password=TEST_PASSWORD),
+            json=payload,
+        )
