ec761d74785e70fc3a93cc5f2ad4f17982f640d5
khalid <khalid619lawal@gmail.com>
2025-02-18 09:24:43 +0000

    test keycloak connection url in git CI

--- src/api/environment.py
+++ src/api/environment.py
@@ -1,7 +1,7 @@
 import os
 
 host = os.environ.get("DATABASE_HOST", "localhost")
-
+keycloak_host = os.environ.get("KEYCLOA_HOST", "localhost")
 # Location of the database
 DB_NAME = "mast_db"
 SQLALCHEMY_DATABASE_URL = f"postgresql://root:root@{host}:5432/{DB_NAME}"
@@ -9,7 +9,7 @@ SQLALCHEMY_DATABASE_URL = f"postgresql://root:root@{host}:5432/{DB_NAME}"
 SQLALCHEMY_DEBUG = os.environ.get("SQLALCHEMY_DEBUG", False)
 
 # Keycloak server url
-SERVER_URL = "http://keycloak:8080"
+SERVER_URL = f"http://{keycloak_host}:8080"
 # Realm name within keycloak
 REALM_NAME = "realm1"
 # Client name withing keycloak
