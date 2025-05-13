223966871197575b9faf88b5df04028bd9d213af
Daniel Brennand <danielabrennand@outlook.com>
2025-05-13 09:08:36 +0100

    Removed certbot post hook bootstrapping

--- dev/docker/bootstrap_nginx.sh
+++
@@ -1,5 +0,0 @@
-echo Starting Nginx Config update;
-cp /nginx_conf/default.conf /nginx_conf/temp.conf;
-sed 's/self_signed/mastapp.site/g' /nginx_conf/temp.conf > /nginx_conf/default.conf;
-rm /nginx_conf/temp.conf;
-echo Finished Nginx Config Update;
